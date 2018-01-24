73d5b4df6519e1dfe6be600aeebe50ddddbadbfc
Alexander <english3000.org@gmail.com>
2018-01-23 21:23:33 -0800

    working Go tests in both test & example syntax!

--- practice-problems/goTesting/practice.go
+++ practice-problems/goTesting/practice.go
@@ -1,21 +1,45 @@
-package main
-
-func main() {
-	strStr("hello", "ll")
-	strStr("jello", "hi")
-  strStr("", "")
-}
+package practice
 
 func strStr(haystack string, needle string) int {
-  needle_length := len(needle)
-  for i := 0; i <= len(haystack) - needle_length; i++ {
-    if haystack[i:i + needle_length] == needle {
-      return i
-    }
-  }
-  return -1
+	needleLength := len(needle)
+	for i := 0; i <= len(haystack)-needleLength; i++ {
+		if haystack[i:i+needleLength] == needle {
+			return i
+		}
+	}
+	return -1
 }
 
 // func MinAbsDiff()  {
 //
 // }
+
+/* Answer: https://www.quora.com/Given-a-matrix-represented-as-int-n-n-rotate-it-90-degrees-clockwise-in-place-In-place-means-minimal-extra-memory-to-be-used-ie-dont-make-a-new-array-to-copy-into
+
+
+func main() {
+	input := [][]int{
+	  []int{1, 2, 3},
+	  []int{4, 5, 6},
+	  []int{7, 8, 9},
+	}
+	rotate(input)
+}
+
+//going from outer layers to inner layers
+func rotateMatrixInplace(matrix [][]int) {
+	length := len(matrix) - 1;
+
+	for i := 0; i <= (length)/2; i++ { //left bound
+		for j := i; j < length-i; j++ { //right bound
+
+			int temp = matrix[i][j];
+
+			matrix[i][j] = matrix[length-j][i];
+			matrix[length-j][i] = matrix[length-i][length-j];
+			matrix[length-i][length-j] = matrix[j][length-i];
+			matrix[j][length-i] = temp;
+		}
+	}
+}
+*/
--- practice-problems/goTesting/practice_test.go
+++ practice-problems/goTesting/practice_test.go
@@ -5,18 +5,51 @@
 
 // import "testing"
 
-package main
+package practice
 
 import (
-  "fmt"
+	"fmt"
+	"testing"
 )
 
-func ExampleHello() {
-  result1 := strStr("hello", "ll")
+var cases = []struct {
+	str      string
+	substr   string
+	expected int
+}{
+	{
+		str:      "hello",
+		substr:   "ll",
+		expected: 2,
+	}, {
+		str:      "jello",
+		substr:   "he",
+		expected: -1,
+	}, {
+		str:      "",
+		substr:   "",
+		expected: 0,
+	},
+}
+
+//test syntax
+func TestStrStr(tests *testing.T) {
+	for _, test := range cases {
+		actual := strStr(test.str, test.substr)
+		if test.expected != actual {
+			tests.Errorf("strStr(%v, %v)\nEXPECTED: %v\nNOT: %v",
+				test.str, test.substr, test.expected, actual)
+		}
+	}
+}
+
+//example syntax
+func ExampleStrStr() {
+	result1 := strStr("hello", "ll")
 	result2 := strStr("jello", "hi")
-  result3 := strStr("", "")
-  fmt.Printf("%v, %v, %v", result1, result2, result3)
-  // Output: 2, -1, 0
+	result3 := strStr("", "")
+	fmt.Printf("%v, %v, %v", result1, result2, result3)
+	// Output: 2, -1, 0
 }
 
 // func TestStrStr(t *testing.T) {
